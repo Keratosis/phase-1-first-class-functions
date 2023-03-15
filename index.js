b01fdc7bdccf59455fde3b60d5ed657558be1a71
braxtonshaffie <braxtonshaffie@gmail.com>
2023-03-15 21:57:05 +0300

    done with assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback){
+    callback()
+    
+}
+callback()
+
+function returnsANamedFunction() {
+    
+    function namedFunction() {
+      console.log("Hello, world!");
+    }
+  
+    return namedFunction;
+  }
+
+  function returnsAnAnonymousFunction() {
+    
+    return function() {
+      console.log("Hello, world!");
+    };
+  }
